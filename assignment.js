b369f795d5d425510ef1af3b990abc1d08cfb0c4
Maskuruzzaman95 <maskuruzzaman95@gmail.com>
2021-01-22 10:34:56 +0600

    added file

---
+++ assignment.js
@@ -0,0 +1,8 @@
+function kilometerToMeter(kilo) {
+    var Meter = kilo*1000; // 1 kilometer = 1000 meter thats why kilo multiply with 1000
+    return(Meter);
+}
+var run = kilometerToMeter(10); //find out answer kilometer to metter 
+console.log(run)
+var run = kilometerToMeter(5);
+console.log(run)
